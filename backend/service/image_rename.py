4c1156721a875667be23e5fffb4d4761ccaeed45
Jaymie <jaymiearclight@gmail.com>
2025-10-30 04:52:01 -0400

    Refactor RenameService class

--- backend/service/image_rename.py
+++ backend/service/image_rename.py
@@ -3,30 +3,28 @@ from utils.file_util import is_image
 import shutil
 import difPy
 
-class rename_service:
-    def __init__(self, load_path="", save_path=""):
-        self.load_path = Path(load_path)
-        self.save_path = Path(save_path)
+class RenameService:
+    def __init__(self, load_path: Path="", save_path:Path=""):
+        self.load_path = load_path
+        self.save_path = save_path
     
     def list_files(self):
         file_list = []
         for file in self.load_path.iterdir():
             file_list.append(file)
         return file_list
-    
+
     def rename_sequential(self, skip_duplicates=False):
         for idx, file in enumerate(self.get_valid_files(skip_duplicates), start=1):
             new_name = f"{idx}{file.suffix}"
             new_path = self.save_path / new_name
             shutil.copy2(file, new_path)
-        print("Rename Complete!")
     
     def rename_stem_sequential(self, skip_duplicates=False):
         for idx, file in enumerate(self.get_valid_files(skip_duplicates), start=1):
             new_name = f"{file.stem}_{idx}{file.suffix}"
             new_path = self.save_path / new_name
             shutil.copy2(file, new_path)
-        print("Rename Complete!")
                 
     def skip_duplicates(self):
         dif = difPy.build(str(self.load_path), recursive=False)
